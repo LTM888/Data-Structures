5a8264992d9a09c2f4564b8798e9fb219838e02e
[your <leigh.tm@gmail.com>
2019-09-20 10:08:45 -0400

    first push on tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -4,48 +4,78 @@ from dll_queue import Queue
 from dll_stack import Stack
 
 class BinarySearchTree:
-  def __init__(self, key):
+  def __init__(self, value):
+    self.value = value
     self.left = None
     self.right = None
-    self.val = key
 
-  # Insert the given value into the tree
-  def insert(root, node):
-    if root is None: 
-        root = node 
-    else: 
-        if root.val < node.val: 
-            if root.right is None: 
-                root.right = node 
-            else: 
-                insert(root.right, node) 
-        else: 
-            if root.left is None: 
-                root.left = node 
-            else: 
-                insert(root.left, node)
+  # Insert the given value into the tree / adds the input value to the binary tree
+  # adhering to the rules of the ordering of the elements in dinary search tree.
+  # Needs to travers to find spot to insert
 
-  # Return True if the tree contains the value
-  # False if it does not
+  def insert(self, value):
+        # Current node is self
+        #Check if self.value is bigger or smaller then new value - left or right
+        if value < self.value:
+              # We go left or right then check if nodeexists
+              if not self.left:
+                    # if node does not exiist then create a node there
+                    self.left = BinarySearchTree(value)
+                    # if node does not exist use recursion: call insert on that node
+              else:
+                    self.left.insert(value)
+        else:
+                  if not self.right:
+                        self.right = BinarySearchTree(value)
+                  else:
+                        self.right.insert(value)
+ 
+
+  
 
   def contains(self, target):
-    if root: 
-        inorder(root.left) 
-        print(root.val) 
-        inorder(root.right)
+    # Check if the current value is the target
+    if self.value == target:
+          return True
+          # Otherwiaw, left or right bases on bigger or smaller, and then call contains agine
+    else:
+      if target < self.value:
+                if not self.left:
+                    return False
+                else:
+                    return self.left.contains(target)
+      else:
+                      # go right
+                if not self.right:
+                    return False
+                else:
+                    self.right.contains(target)
 
   # Return the maximum value found in the tree
   def get_max(self):
-    pass
+    # max node is fartherst to the right
+    if not self.right:
+          return self.value
+    return self.right.get_max()
+
+
+
 
   # Call the function `cb` on the value of each node
   # You may use a recursive or iterative approach
-  def r_for_each(self, cb):
-    pass
+  def for_each(self, cb):
+    cb(self.value)
+    if self.left:
+        self.left.for_each(cb)
+    if self.right:
+        self.right.for_each(cb)
+              
+        
+    
 
   # Print all the values in order from low to high
   # Hint:  Use a recursive, depth first traversal
-  def in_order_print()
+  def in_order_print():
     pass
 
   # Print the value of every node, starting with the given node,
